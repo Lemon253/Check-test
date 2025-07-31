2d04970677923bba08872444bf49ff4c64e040e6
gotouge <gotouge@gmail.com>
2025-08-01 06:29:59 +0900

    バリデーションの設定追加

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\ContactRequest;
 use App\Models\Contact;
 use App\Models\Category;
 
@@ -14,7 +14,7 @@ class ContactController extends Controller
         return view('index');
     }
 
-    public function confirm(Request $request)
+    public function confirm(ContactRequest $request)
     {
         $contact = $request->only(['name','email','tel','content']);
         //return view('confirm', ['contact' => $contact]);
@@ -22,7 +22,7 @@ class ContactController extends Controller
 
     }
 
-    public function store(Request $request)
+    public function store(ContactRequest $request)
     {
         $contact = $request->only(['name', 'email', 'tel', 'content']);
 
